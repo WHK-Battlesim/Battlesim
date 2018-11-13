a3644bd1722299e6bcda3875487bf3d4a283f276
Lukas Wagner <lukas.wagner@student.hpi.de>
2018-11-13 19:59:42 +0100

    adapted mapgenerator to read new maps

--- Battlesim/Assets/Scripts/MapGenerator.cs
+++ Battlesim/Assets/Scripts/MapGenerator.cs
@@ -1,28 +1,78 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using TriangleNet.Geometry;
 using TriangleNet.Meshing;
+using TriangleNet.Topology;
 
 namespace Assets.Scripts
 {
     public class MapGenerator : MonoBehaviour
     {
         public Texture2D DefaultHeightMap;
+        public Texture2D DefaultFeatureMap;
+        public TextAsset DefaultExtents;
         public Material Material;
 
         public Vector3 Offset = Vector3.zero;
-        public Vector3 Scale = new Vector3(10, 1, 10);
+        public float Scale = 10;
         public int NumberOfVertices = 10000;
 
-        public Color GrassColor = new Color(0.516f, 0.867f, 0.279f);
-        public Color WaterColor = new Color(0.2f, 0.2f, 0.8f);
-
-        private void Start ()
+        [Serializable]
+        public struct TerrainFeature
         {
-            BuildTerrain(DefaultHeightMap);
+            [HideInInspector]
+            public string Name;
+            public Color FeatureMapColor;
+            public Color MeshColor;
+
         }
 
-        public void BuildTerrain(Texture2D heightMap)
+        public List<TerrainFeature> TerrainFeatures = new List<TerrainFeature>()
+        {
+            new TerrainFeature()
+            {
+                Name = "Land",
+                FeatureMapColor = Color.white,
+                MeshColor = new Color(0.516f, 0.867f, 0.279f)
+            },
+            new TerrainFeature()
+            {
+                Name = "River",
+                FeatureMapColor = Color.green,
+                MeshColor = new Color(0.2f, 0.2f, 0.8f)
+            },
+            new TerrainFeature()
+            {
+                Name = "Lake",
+                FeatureMapColor = Color.red,
+                MeshColor = new Color(0.2f, 0.2f, 0.8f)
+            },
+            new TerrainFeature()
+            {
+                Name = "Fallback",
+                FeatureMapColor = Color.magenta,
+                MeshColor = Color.magenta
+            },
+        };
+
+        private void Start()
+        {
+            BuildTerrain(DefaultHeightMap, DefaultFeatureMap);
+        }
+
+        private int GetFeatureId(Texture2D featureMap, Triangle triangle)
+        {
+            var avgX = (triangle.vertices[0].X + triangle.vertices[1].X + triangle.vertices[2].X) / 3;
+            var avgY = (triangle.vertices[0].Y + triangle.vertices[1].Y + triangle.vertices[2].Y) / 3;
+            var color = featureMap.GetPixel((int)Math.Round(featureMap.width * avgX),
+                                            (int)Math.Round(featureMap.height * avgY));
+            var index = TerrainFeatures.FindIndex(terrainFeature => terrainFeature.FeatureMapColor == color) % TerrainFeatures.Count;
+            return index;
+        }
+
+        public void BuildTerrain(Texture2D heightMap, Texture2D featureMap)
         {
             var terrain = new GameObject("Terrain");
             terrain.transform.SetParent(transform);
@@ -32,7 +82,7 @@ namespace Assets.Scripts
 
             var mesh = meshFilter.mesh;
             mesh.Clear();
-            
+
             var polygon = new Polygon();
             var random = new System.Random();
             for (var i = 0; i < NumberOfVertices; i++)
@@ -40,15 +90,49 @@ namespace Assets.Scripts
                 polygon.Add(new Vertex(random.NextDouble(), random.NextDouble()));
             }
 
-            var options = new ConstraintOptions {ConformingDelaunay = true};
+            var options = new ConstraintOptions { ConformingDelaunay = true };
             var triangulatedMesh = polygon.Triangulate(options);
 
-            mesh.SetVertices(triangulatedMesh.Vertices.Select(v => Vector3.Scale(new Vector3((float) v.X, heightMap.GetPixelBilinear((float) v.X, (float) v.Y).r, (float) v.Y), Scale) + Offset).ToList());
-            mesh.SetTriangles(triangulatedMesh.Triangles.SelectMany(t => t.vertices.Reverse(), (t, v) => v.ID).ToArray(), 0);
-            mesh.RecalculateNormals();
+            mesh.SetVertices(
+                triangulatedMesh
+                    .Vertices
+                    .Select(
+                        vertex => new Vector3(
+                                 (float)vertex.X,
+                                 heightMap.GetPixelBilinear((float)vertex.X, (float)vertex.Y).r,
+                                 (float)vertex.Y)
+                             * Scale + Offset)
+                    .ToList());
 
-            meshRenderer.material = Material;
-            meshRenderer.material.color = GrassColor;
+            var triangleGroupVertices =
+                triangulatedMesh
+                    .Triangles
+                    .GroupBy(
+                        triangle => GetFeatureId(featureMap, triangle))
+                    .Select(
+                        grouping => new Tuple<int, IEnumerable<int>>(
+                            grouping.Key,
+                            grouping.SelectMany(
+                                triangle => triangle.vertices.Reverse(),
+                                (triangle, vertex) => vertex.ID)))
+                    .ToList();
+
+            var triangleGroupCount = triangleGroupVertices.Count();
+            mesh.subMeshCount = triangleGroupCount;
+
+            var materials = new Material[triangleGroupCount];
+            foreach (var vertexGroup in triangleGroupVertices)
+            {
+                mesh.SetTriangles(vertexGroup.Item2.ToArray(), vertexGroup.Item1);
+
+                materials[vertexGroup.Item1] = new UnityEngine.Material(Material)
+                {
+                    color = TerrainFeatures[vertexGroup.Item1].MeshColor
+                };
+            }
+
+            meshRenderer.materials = materials;
+            mesh.RecalculateNormals();
         }
     }
 }
