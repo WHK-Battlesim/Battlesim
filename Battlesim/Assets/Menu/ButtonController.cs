dfdd0d5ae360c71ea2d74ff6a87163b7c25115ef
leogeier <leonard.geier@hotmail.com>
2018-11-19 18:00:24 +0100

    Add scroll view with buttons

---
+++ Battlesim/Assets/Menu/ButtonController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour {
+
+    public GameObject buttonPrefab;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 5; i++) {
+            GenerateButton("Button" + i.ToString());
+        }
+	}
+	
+    void GenerateButton(string text) {
+        GameObject button = Instantiate(buttonPrefab);
+        button.transform.parent = gameObject.transform;
+        button.GetComponentInChildren<Text>().text = text;
+
+        button.GetComponent<Button>().onClick.AddListener(
+            () => ButtonClick(text));
+    }
+
+    void ButtonClick(string text) {
+        Debug.Log(text);
+    }
+}
