aaced6ec9018e0c36fa6d635a7531c5ad9e1684a
Lukas Wagner <lukas.wagner@student.hpi.de>
2018-11-25 21:20:56 +0100

    animate unit movement

--- Battlesim/Assets/Scripts/Unit.cs
+++ Battlesim/Assets/Scripts/Unit.cs
@@ -3,6 +3,8 @@ using UnityEngine.AI;
 
 namespace Assets.Scripts
 {
+    [RequireComponent (typeof (NavMeshAgent))]
+    [RequireComponent (typeof (Animator))]
     public class Unit : MonoBehaviour
     {
         public int MaxHealth;
@@ -16,6 +18,7 @@ namespace Assets.Scripts
         public double AreaDamage;
 
         private NavMeshAgent _agent;
+        private Animator _animator;
         private int _health;
         private double _moral;
 
@@ -30,6 +33,30 @@ namespace Assets.Scripts
         {
             Prussia,
             Austria
+        }
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+            _agent = GetComponent<NavMeshAgent>();
+
+            _agent.updatePosition = false;
+        }
+
+        private void Update()
+        {
+            var moving = _agent.remainingDistance > _agent.radius;
+            _animator.SetBool("Moving", _agent.remainingDistance > _agent.radius);
+            if (!moving)
+            {
+                _agent.velocity = Vector3.zero;
+            }
+        }
+
+        private void OnAnimatorMove()
+        {
+            if(_agent!=null)
+                transform.position = _agent.nextPosition;
         }
     }
 }
