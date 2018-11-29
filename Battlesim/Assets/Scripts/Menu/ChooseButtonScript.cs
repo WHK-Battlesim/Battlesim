50b3a6427bffcbe5f0d89f2b3dfb74c9b1f09b1b
leogeier <leonard.geier@hotmail.com>
2018-11-29 01:14:03 +0100

    Switch to other scene

---
+++ Battlesim/Assets/Scripts/Menu/ChooseButtonScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChooseButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Button>().onClick.AddListener(
+            () => SceneManager.LoadScene("Situation Menu")
+        );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
